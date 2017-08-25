d39270748dc027571716def1e6deb6ae7112f0b1
chenliujin <liujin.chen@qq.com>
2017-08-25 16:30:27 +0800

    mysqldump

--- bin/mysqldump_parking.sh
+++ bin/mysqldump_parking.sh
@@ -6,11 +6,18 @@ user='appuser'
 passwd='123456'
 database='parking_meter'
 table='parking_tariff parking_tariff_range_rule parking_tariff_tag_match parking_price_table'
-output_file=$database'.parking_tariff.sql'
+output_file=$database'.sql'
 
 # 1. 不导出数据：--no-data
 # 1. 不需要 drop table: --skip-add-drop-table
 # 1. 去掉 AUTO_INCREMENT
 # 1. 只导出数据，不需要创建表：--no-create-info
-mysqldump -h$host -P$port -u$user -p$passwd --skip-add-drop-table --no-data $database $table | sed 's/ AUTO_INCREMENT=[0-9]*//' > $output_file
+mysqldump \
+	-h$host \
+	-P$port \
+	-u$user \
+	-p$passwd \
+	--skip-add-drop-table \
+	--no-data \
+	$database | sed 's/ AUTO_INCREMENT=[0-9]*//' > $output_file
 
